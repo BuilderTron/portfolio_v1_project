cdc64bade049a6c1c09b2e6bd15dd740fcbff69c
Jose Lopez <jj.lopezbuilder@gmail.com>
2020-02-29 00:41:54 +0000

    added static settings

--- portfolio_v1/settings.py
+++ portfolio_v1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h8%f1w-gfn_516&28gl#9re9)2e@!&jk$ix^7u14+h7@wj(luf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jjlopez.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
